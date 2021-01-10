81c90eecab80cfaaa8dda3c2abbb1606b104427a
Jason J Cruz <jaycru06@yahoo.com>
2021-01-10 03:57:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const fi = (function() {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-    // Iterates over a collection of elements, passing each element in turn to a callback function. Each invocation of callback is called with three arguments: (element, index, collection). If collection is a JavaScript object, callback's arguments will be (value, key, collection). Returns the original collection for chaining.
-
 
     each: function(collection, callback) {
       for (const key in collection) {
